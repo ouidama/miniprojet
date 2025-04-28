737dbeba866bc6d86f7e5ce6e3991f9ebb921f44
ouidama <ouidani.ma@outlook.com>
2025-04-28 14:00:37 +0100

    finish ajout et affichage

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define STRING_MAX 255
 #define MAX_LIVRES 255
+
+// get current year
+int annee;
 
 struct livre {
 	int id;
@@ -12,27 +16,54 @@ struct livre {
 };
 
 struct livre livres_t[MAX_LIVRES];
-int nb_livres=0;
+int nb_livres = 0;
 
-void ajout_livre(){
-	struct livre *l=&livres_t[nb_livres];
+void ajout_livre() {
+	if (nb_livres >= MAX_LIVRES) {
+		printf("Vous avez atteint le nombre maximum de livres.\n");
+		return;
+	}
+
+	struct livre *l = &livres_t[nb_livres];
+
 	do {
-		printf("Saisir l identifiant : ");
+		printf("Saisir l'identifiant : ");
 		scanf("%d",&l->id);
-	} while(l->id<0);
+	} while(l->id < 0);
+
+	printf("Saisir le titre : ");
+	scanf("%s", &l->titre);
+
+	printf("Saisir le auteur : ");
+	scanf("%s", &l->auteur);
+
+	do {
+		printf("Saisir la date : ");
+		scanf("%d", &l->date);
+	} while(l->date <= 0 || l->date > annee);
+
 	nb_livres++;
 }
 
-void aff_livres(){
-	for(int i = 0; i < nb_livres; i++) {
+void aff_livres() {
+	if (nb_livres == 0) {
+		printf("Il y'a aucun livre.\n");
+		return;
+	}
+
+	printf("ID\tTitre\tAuteur\tdate\n");
+	for (int i = 0; i < nb_livres; i++) {
 		struct livre *l=&livres_t[i];
 		printf("%d\t%s\t%s\t%d\n",l->id,l->titre,l->auteur,l->date);
 	}
 }
 
 int main() {
-	ajout_livre();
-	strcpy(livres_t[0].titre,"test");
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	annee = tm.tm_year + 1900;
+
+	//ajout_livre();
 	aff_livres();
 	return 0;
 }
