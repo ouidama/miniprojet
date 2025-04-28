ad02c3ad81fb82fa80ee5f320f4dbeb85a71555f
ouidama <ouidani.ma@outlook.com>
2025-04-28 14:56:13 +0100

    suppression

--- main.c
+++ main.c
@@ -58,12 +58,46 @@ void aff_livres() {
 	}
 }
 
+void sup_livre() {
+	if (nb_livres == 0) {
+		printf("Il y'a aucun livre.\n");
+		return;
+	}
+
+	int id, trouve;
+	trouve = 0;
+	do {
+		printf("Saisir l'identifiant du livre a supprimer : ");
+		scanf("%d", &id);
+	} while(id < 0);
+
+	for (int i = 0; i < nb_livres; i++) {
+		if (id == livres_t[i].id) {
+			trouve = 1;
+			for (int j = i; j < nb_livres - 1; j++) {
+				livres_t[j] = livres_t[j+1];
+			}
+			nb_livres--;
+		}
+	}
+
+	if (trouve) {
+		printf("Le livre avec cet identifiant a ete suprimmer.\n");
+	} else {
+		printf("Aucun livre avec cet identifiant existe.\n");
+	}
+}
+
 int main() {
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
 	annee = tm.tm_year + 1900;
 
-	//ajout_livre();
+	ajout_livre();
+	ajout_livre();
+	ajout_livre();
+	aff_livres();
+	sup_livre();
 	aff_livres();
 	return 0;
 }
