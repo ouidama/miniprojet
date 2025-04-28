6f03f18fa50d41f52924e466134bbd60ab151ae0
ouidama <ouidani.ma@outlook.com>
2025-04-28 12:57:06 +0100

    struct et tableau

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+#define STRING_MAX 255
+#define MAX_LIVRES 255
+
+struct livre {
+	int id;
+	char titre[STRING_MAX];
+	char auteur[STRING_MAX];
+	int date;
+};
+
+struct livre livres_t[MAX_LIVRES];
+
 int main() {
 	return 0;
 }
